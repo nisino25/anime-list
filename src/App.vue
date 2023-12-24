a88dd9f5109ac55ca030c623e29db0119a9c751c
nisino25 <nisino25@gmail.com>
2023-12-25 00:54:54 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, voluptatum. Alias ullam, ducimus nam, mollitia hic unde quisquam nulla earum voluptatem adipisci aspernatur maiores quidem sunt. Nesciunt quaerat accusantium illo, error enim aliquam eaque atque placeat commodi ab similique at.</span>
+</template>
+
+<script>
+  // import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style>
+  /* #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  } */
+
+  html{
+    background: #F7EADF;
+    font-family: 'Roboto Condensed', sans-serif;
+    /* #FB6350 */
+  }
+</style>
