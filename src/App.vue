09afca43b9808c00abf75b70f206a80c4ae95574
nisino25 <nisino25@gmail.com>
2023-12-25 01:26:08 +0900

    anilist basic

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, voluptatum. Alias ullam, ducimus nam, mollitia hic unde quisquam nulla earum voluptatem adipisci aspernatur maiores quidem sunt. Nesciunt quaerat accusantium illo, error enim aliquam eaque atque placeat commodi ab similique at.</span>
+    <div>
+    <h1>Anime Details</h1>
+    <div v-if="loading">Loading...</div>
+    <div v-else-if="animeData">
+      <h2>{{ animeData.title.romaji }}</h2>
+      <p>English Title: {{ animeData.title.english }}</p>
+      <p>Native Title: {{ animeData.title.native }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,6 +15,55 @@
 
   export default {
     name: 'App',
+    
+    data() {
+      return {
+        animeData: null,
+        loading: false,
+        error: null
+      };
+    },
+    methods: {
+      async fetchAnimeData() {
+        this.loading = true;
+        const query = `
+          query ($id: Int) {
+            Media (id: $id, type: ANIME) {
+              id
+              title {
+                romaji
+                english
+                native
+              }
+            }
+          }
+        `;
+        const variables = { id: 15125 }; // You can change the ID to fetch different anime
+        try {
+          const response = await fetch('https://graphql.anilist.co', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+            },
+            body: JSON.stringify({ query, variables })
+          });
+          const jsonResponse = await response.json();
+          if (jsonResponse.errors) {
+            throw new Error('Error fetching data');
+          }
+          this.animeData = jsonResponse.data.Media;
+        } catch (error) {
+          console.error('Fetch error:', error);
+          this.error = error;
+        } finally {
+          this.loading = false;
+        }
+      }
+    },
+    mounted() {
+      this.fetchAnimeData();
+    }
   }
 </script>
 
